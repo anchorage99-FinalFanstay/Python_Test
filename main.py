943304db7e7b7377a1fb5e8af8408c020f6957f5
Your Name <anchorage99@gmail.com>
2026-02-24 17:13:44 -0500

    Second 2 Commit

--- main.py
+++ main.py
@@ -4,13 +4,12 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def in_autotests_we_trust(a, b):
+    if a == b:
+        print('Test passed')
+    else:
+        print('Test failed')
 
+in_autotests_we_trust(10, '10')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+in_autotests_we_trust(0, False)
